9b89d23a35f2399b607f2be7b39a58a69dc7ff90
tolik <tolik.otkirovich@mail.ru>
2024-11-18 23:53:07 +0500

    LeetCode: IncreasingTripletSubsequence problem solved

---
+++ src/main/java/com/tolik/chellange75/IncreasingTripletSubsequence.java
@@ -0,0 +1,19 @@
+package com.tolik.chellange75;
+
+public class IncreasingTripletSubsequence {
+
+    public boolean increasingTriplet(int[] nums) {
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if(num<a)
+                a = num;
+            else if(num<b)
+                b = num;
+            else
+                return true;
+        }
+        return false;
+    }
+}
