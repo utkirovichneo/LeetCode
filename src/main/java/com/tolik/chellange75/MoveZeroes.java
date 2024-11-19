77388eef3e05e332fc417f105cb4c5f072af56f8
tolik <tolik.otkirovich@mail.ru>
2024-11-19 18:43:18 +0500

    LeetCode: MoveZeroes problem solved

---
+++ src/main/java/com/tolik/chellange75/MoveZeroes.java
@@ -0,0 +1,15 @@
+package com.tolik.chellange75;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int num : nums) {
+            if(num != 0){
+                nums[index++] = num;
+            }
+        }
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
