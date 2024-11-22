6e4893ee9862cc4b527cd98c02fc6d9665c147e3
tolik <tolik.otkirovich@mail.ru>
2024-11-23 00:16:23 +0500

    LeetCode: ShuffletheArray problem solved

---
+++ src/main/java/com/tolik/math/ShuffletheArray.java
@@ -0,0 +1,12 @@
+package com.tolik.math;
+
+public class ShuffletheArray {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[2*n];
+        for (int i = 0; i < n; i++) {
+            result[2*i] = nums[i];
+            result[2*i+1] = nums[n + i];
+        }
+        return result;
+    }
+}
