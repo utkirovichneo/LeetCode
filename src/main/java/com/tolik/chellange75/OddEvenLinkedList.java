430ebcafd03a8eef3b2ae92f8f91e0756c7d3aa2
tolik <tolik.otkirovich@mail.ru>
2024-11-25 17:51:09 +0500

    LeetCode: OddEvenLinkedList problem solved

---
+++ src/main/java/com/tolik/chellange75/OddEvenLinkedList.java
@@ -0,0 +1,49 @@
+package com.tolik.chellange75;
+
+public class OddEvenLinkedList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    public static ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while(even.next != null) {
+                odd.next = even.next;
+                odd = odd.next;
+                even.next = odd.next;
+                even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        listNode.next.next.next.next = new ListNode(5);
+
+        ListNode listNode1 = oddEvenList(listNode);
+        System.out.println(listNode1);
+    }
+
+}
