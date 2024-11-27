38386686373176e91dad85ffbcff950adab67e19
tolik <tolik.otkirovich@mail.ru>
2024-11-27 14:13:43 +0500

    LeetCode: FindCenterofStarGraph problem solved

---
+++ src/main/java/com/tolik/problems/FindCenterofStarGraph.java
@@ -0,0 +1,7 @@
+package com.tolik.problems;
+
+public class FindCenterofStarGraph {
+    public int findCenter(int[][] edges) {
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+    }
+}
