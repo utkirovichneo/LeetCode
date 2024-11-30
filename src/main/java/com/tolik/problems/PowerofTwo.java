932cc74dcdac80be809eb5efc67419ea36add3a5
tolik <tolik.otkirovich@mail.ru>
2024-11-30 23:35:46 +0500

    LeetCode: PowerofTwo problem solved

---
+++ src/main/java/com/tolik/problems/PowerofTwo.java
@@ -0,0 +1,11 @@
+package com.tolik.problems;
+
+public class PowerofTwo {
+        public boolean isPowerOfTwo(int n) {
+        for (int i = 0; i < 31; i++) {
+            if(n == Math.pow(2, i))
+                return true;
+        }
+        return false;
+    }
+}
