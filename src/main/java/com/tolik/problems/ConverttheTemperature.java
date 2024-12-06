c467a7ceffbb782fa88708a3f95c2ea48202ff01
tolik <tolik.otkirovich@mail.ru>
2024-12-06 13:49:18 +0500

    LeetCode: ConverttheTemperature problem solved

---
+++ src/main/java/com/tolik/problems/ConverttheTemperature.java
@@ -0,0 +1,7 @@
+package com.tolik.problems;
+
+public class ConverttheTemperature {
+    public double[] convertTemperature(double celsius) {
+        return new double[] {celsius + 273.15, celsius * 1.8 + 32D};
+    }
+}
