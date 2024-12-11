946461afad6925d3c2ad393771e79f1e2c01e506
tolik <tolik.otkirovich@mail.ru>
2024-12-11 21:42:05 +0500

    LeetCode: RunningSumof1dArray problem solved

---
+++ src/main/java/com/tolik/problems/RunningSumof1dArray.java
@@ -0,0 +1,10 @@
+package com.tolik.problems;
+
+public class RunningSumof1dArray {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
